82f0d149bcbfa28d34ec16c1b449c10d9a50e536
nATOr <gowluonade@gmail.com>
2023-06-22 07:11:45 -0500

    cheerio-scraping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-export default function Home () {
+import { load } from 'cheerio'
+
+async function getChallenges () {
+  interface NextData {
+    props: {
+      initialState: {
+        'v2/challenges': {
+          entities: Record<string, { title: string, description: string }>
+        }
+      }
+    }
+  }
+
+  const html = await fetch('https://www.frontendmentor.io/challenges').then(res => res.text())
+  const $ = load(html)
+  const nextData = JSON.parse($('#__NEXT_DATA__').html() as string) as NextData
+  return Object.values(nextData.props.initialState['v2/challenges'].entities)
+}
+export default async function Home () {
+  const challenges = await getChallenges()
+  console.log(challenges)
   return (
     <main>
-      holis
+      <h1 className='text-xl font-bold mb4'>Frontendmentos Challenges</h1>
+      <ul>
+        {challenges.map(challenge => (
+          <li key={challenge.title}>
+            <h2>{challenge.title}</h2>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
